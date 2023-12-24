5c5f6bbb0b7a92b0ae966e2f91b6eca14855af81
Ektamadaan <ektamadaan45@gmail.com>
2023-12-24 23:58:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 function firstWord(s) {
   // your code here
+	if (str.length === 0) {
+        return str;
+    }
+
+    // Find the index of the first space in the string
+    const spaceIndex = str.indexOf(' ');
+
+    // If no space is found, return the entire string
+    if (spaceIndex === -1) {
+        return str;
+    }
+
+    // Extract and return the first word (characters up to the first space)
+    return str.substring(0, spaceIndex);
 }
 
 // Do not change the code below
 
-const s = prompt("Enter String:");
+//const s = prompt("Enter String:");
 alert(firstWord(s));
